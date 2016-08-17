d2340e89bc4353db9c8bf66ec04c2808404ad104
Aliaksandr Kazarez <kozorezal@gmail.com>
2016-08-18 00:09:39 +0300

    add version to logging messages

--- main.js
+++ main.js
@@ -5,12 +5,12 @@
 console.warn("MAIN: Hello, I'm main");
 console.log("I'm depending on @dependencies-test/test@1 and @dependencies-test/secret@2 and @dependencies-test/foo@1");
 
-console.info("MAIN: importing test");
+console.info("MAIN: importing test@1");
 require("@dependencies-test/test");
 
-console.info("MAIN: importing secret");
+console.info("MAIN: importing secret@2");
 var f = require("@dependencies-test/secret");
 f();
 
-console.info("MAIN: importing foo");
+console.info("MAIN: importing foo@1");
 require("@dependencies-test/foo");
